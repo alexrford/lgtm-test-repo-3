72e82c85cd339ffa02d77235bd7222fce20d846f
Sam Lanning <sam@semmle.com>
2016-09-16 15:06:17 +0100

    fail java!!

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ class Main {
 	private String baz;
 
 	public static void main(String[] args) {
-		System.out.println("Hello World" + new Main().baz);
+		System.out.println("Hello World" + new Main().baz)
 		System.out.println("Hello World" + new Main().baz);
 		System.out.println("Hello World" + new Main().baz);
 
