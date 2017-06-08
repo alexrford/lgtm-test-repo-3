51878442a0a20f85c47f150c1a1477cea5e44f52
Alexander Ford <alexf@semmle.com>
2017-06-08 18:43:12 +0100

    add file

---
+++ bar.py
@@ -0,0 +1,13 @@
+def fun(y):
+    x = 1
+    if y == 1:
+        x = 2
+    elif y == 2:
+        x = 3
+    elif y == 3:
+        x = 4
+    elif y == 4:
+        x = 5
+    else:
+        x = 6
+    print(x)
